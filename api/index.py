2ea2d0bcee31434ff90664fd3daec0a1fa9d0907
nayakshubham2 <22f1001034@ds.study.iitm.ac.in>
2025-05-25 18:54:53 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -21,7 +21,11 @@ with open(MARKS_FILE) as f:
     marks_db = {entry["name"]: entry["marks"] for entry in marks_list}
 
 @app.get("/")
-async def root(request: Request):
+async def root():
+    return JSONResponse(content={"marks": marks_db})
+
+@app.get("/api")
+async def marks(request: Request):
     names = request.query_params.getlist("name")
     result = [marks_db.get(name, 0) for name in names]
     return JSONResponse(content={"marks": result})
